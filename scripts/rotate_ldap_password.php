58ff5a4817e7419c2d1cc6e8d5dcc4176548f0cb
Franco Springveldt <franco@silverstripe.com>
2017-09-12 09:48:07 +1200

    PSR2 linting fixes

--- scripts/rotate_ldap_password.php
+++ scripts/rotate_ldap_password.php
@@ -33,11 +33,11 @@ error_reporting(E_ALL | E_STRICT);
  * rotate_ldap_password /sites/_ss_environment.php ldaps://otherldap.org PRIVILEGED_DN PRIVILEGED_PASSWORD TARGET_DN TARGET_PASSWORD
  */
 
-if(count($_SERVER['argv'])!=5 && count($_SERVER['argv'])!=7) {
-	echo "Not enough arguments.\n\n";
-	echo "Usage (`c_' suffix denotes constants, not actual value):\n";
-	echo "	{$_SERVER['argv'][0]} <ss_env_file> <c_hostname|hostname> <c_privileged_dn> <c_privileged_password> [<c_target_dn> <c_target_password>]\n";
-	exit(1);
+if (count($_SERVER['argv'])!=5 && count($_SERVER['argv'])!=7) {
+    echo "Not enough arguments.\n\n";
+    echo "Usage (`c_' suffix denotes constants, not actual value):\n";
+    echo "	{$_SERVER['argv'][0]} <ss_env_file> <c_hostname|hostname> <c_privileged_dn> <c_privileged_password> [<c_target_dn> <c_target_password>]\n";
+    exit(1);
 }
 
 $envFilePath = $_SERVER['argv'][1];
@@ -46,22 +46,22 @@ $privilegedDNConst = $_SERVER['argv'][3];
 $privilegedPasswordConst = $_SERVER['argv'][4];
 
 if (empty($_SERVER['argv'][5])) {
-	// No target user specified, assume the privileged user's password is being changed.
-	$DNConst = $privilegedDNConst;
-	$passwordConst = $privilegedPasswordConst;
+    // No target user specified, assume the privileged user's password is being changed.
+    $DNConst = $privilegedDNConst;
+    $passwordConst = $privilegedPasswordConst;
 } else {
-	// Target specified for change.
-	$DNConst = $_SERVER['argv'][5];
-	$passwordConst = $_SERVER['argv'][6];
+    // Target specified for change.
+    $DNConst = $_SERVER['argv'][5];
+    $passwordConst = $_SERVER['argv'][6];
 }
 
-if(!file_exists($envFilePath)) {
-	echo sprintf("%s does not exist\n", $envFilePath);
-	exit(1);
+if (!file_exists($envFilePath)) {
+    echo sprintf("%s does not exist\n", $envFilePath);
+    exit(1);
 }
-if(!is_writable($envFilePath)) {
-	echo sprintf("%s is not writable\n", $envFilePath);
-	exit(1);
+if (!is_writable($envFilePath)) {
+    echo sprintf("%s is not writable\n", $envFilePath);
+    exit(1);
 }
 
 require_once($envFilePath);
@@ -72,36 +72,36 @@ ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 ldap_set_option($conn, LDAP_OPT_REFERRALS, 0);
 
 $bind = @ldap_bind($conn, constant($privilegedDNConst), constant($privilegedPasswordConst));
-if(!$bind) {
-	echo sprintf(
-		"Could not bind to LDAP: %s (error code: %s)\n",
-		ldap_error($conn),
-		ldap_errno($conn)
-	);
-	exit(1);
+if (!$bind) {
+    echo sprintf(
+        "Could not bind to LDAP: %s (error code: %s)\n",
+        ldap_error($conn),
+        ldap_errno($conn)
+    );
+    exit(1);
 }
 
 // ldap_read searches the directory but uses LDAP_SCOPE_BASE, so we only get a single entry.
 // We're just doing this to validate that the entry exists in the directory.
 $query = ldap_read($conn, constant($DNConst), '(objectClass=user)', ['dn']);
-if(!$query) {
-	echo sprintf(
-		"Could not read the LDAP entry %s: %s (error code: %s)\n",
-		constant($DNConst),
-		ldap_error($conn),
-		ldap_errno($conn)
-	);
-	exit(1);
+if (!$query) {
+    echo sprintf(
+        "Could not read the LDAP entry %s: %s (error code: %s)\n",
+        constant($DNConst),
+        ldap_error($conn),
+        ldap_errno($conn)
+    );
+    exit(1);
 }
 
 $results = ldap_get_entries($conn, $query);
 
-if(empty($results[0]['dn'])) {
-	echo sprintf(
-		"There was a problem retrieving the details for %s\n",
-		constant($DNConst)
-	);
-	exit(1);
+if (empty($results[0]['dn'])) {
+    echo sprintf(
+        "There was a problem retrieving the details for %s\n",
+        constant($DNConst)
+    );
+    exit(1);
 }
 
 // Generate a sufficiently complex password and set that as the password in LDAP.
@@ -109,29 +109,28 @@ if(empty($results[0]['dn'])) {
 //
 // With 24-character long password, this hardly ever loops.
 for (;;) {
-	$password = generatePassword(24);
-	if (
-		preg_match('/[A-Z]/', $password) &&
-		preg_match('/[a-z]/', $password) &&
-		preg_match('/[0-9]/', $password) &&
-		preg_match('/[\!\@\#\$\%\&\*\?]/', $password)
-	) {
-		break;
-	}
+    $password = generatePassword(24);
+    if (preg_match('/[A-Z]/', $password) &&
+        preg_match('/[a-z]/', $password) &&
+        preg_match('/[0-9]/', $password) &&
+        preg_match('/[\!\@\#\$\%\&\*\?]/', $password)
+    ) {
+        break;
+    }
 }
 
 $success = ldap_modify($conn, $results[0]['dn'], [
-	'unicodePwd' => iconv('UTF-8', 'UTF-16LE', sprintf('"%s"', $password))
+    'unicodePwd' => iconv('UTF-8', 'UTF-16LE', sprintf('"%s"', $password))
 ]);
 
-if(!$success) {
-	echo sprintf(
-		"Could not modify the LDAP entry %s: %s (error code: %s)\n",
-		constant($DNConst),
-		ldap_error($conn),
-		ldap_errno($conn)
-	);
-	exit(1);
+if (!$success) {
+    echo sprintf(
+        "Could not modify the LDAP entry %s: %s (error code: %s)\n",
+        constant($DNConst),
+        ldap_error($conn),
+        ldap_errno($conn)
+    );
+    exit(1);
 }
 
 ldap_close($conn);
@@ -142,40 +141,45 @@ $contents = str_replace(constant($passwordConst), $password, $contents);
 file_put_contents($envFilePath, $contents);
 
 echo sprintf(
-	"LDAP password for %s has been reset. Old password has been replaced in %s\n",
-	$results[0]['dn'],
-	$envFilePath
+    "LDAP password for %s has been reset. Old password has been replaced in %s\n",
+    $results[0]['dn'],
+    $envFilePath
 );
 
 exit(0);
 
-function generatePassword($length = 24) {
-	$passwordData = null;
+function generatePassword($length = 24)
+{
+    $passwordData = null;
 
-	if(function_exists('mcrypt_create_iv')) {
-		$e = mcrypt_create_iv(64, MCRYPT_DEV_URANDOM);
-		if($e !== false) $passwordData = $e;
-	}
+    if (function_exists('mcrypt_create_iv')) {
+        $e = mcrypt_create_iv(64, MCRYPT_DEV_URANDOM);
+        if ($e !== false) {
+            $passwordData = $e;
+        }
+    }
 
-	if(empty($passwordData) && function_exists('openssl_random_pseudo_bytes')) {
-		$e = openssl_random_pseudo_bytes(64, $strong);
-		// Only return if strong algorithm was used
-		if($strong) $passwordData = $e;
-	}
+    if (empty($passwordData) && function_exists('openssl_random_pseudo_bytes')) {
+        $e = openssl_random_pseudo_bytes(64, $strong);
+        // Only return if strong algorithm was used
+        if ($strong) {
+            $passwordData = $e;
+        }
+    }
 
-	if(empty($passwordData)) {
-		echo "Cannot generate passwords - PRNG functions missing? Try installing mcrypt.";
-		exit(1);
-	}
+    if (empty($passwordData)) {
+        echo "Cannot generate passwords - PRNG functions missing? Try installing mcrypt.";
+        exit(1);
+    }
 
-	// Convert the binary data into password-usable characters.
-	$usable = str_replace('=', '', base64_encode($passwordData));
+    // Convert the binary data into password-usable characters.
+    $usable = str_replace('=', '', base64_encode($passwordData));
 
-	// Re-map the base64 alphabet to include special password characters so we can satisfy all character classes.
-	$withSpecial = strtr($usable, '/+abcABC', '!@#$%&*?');
+    // Re-map the base64 alphabet to include special password characters so we can satisfy all character classes.
+    $withSpecial = strtr($usable, '/+abcABC', '!@#$%&*?');
 
-	// Trim to requested size.
-	$password = substr($withSpecial, 0, $length);
+    // Trim to requested size.
+    $password = substr($withSpecial, 0, $length);
 
-	return $password;
+    return $password;
 }
