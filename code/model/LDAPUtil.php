d5c8dcc0165ab52cc55ca3cd3c42808857b153e5
Jakub Dolba <jakub.dolba@silverstripe.com>
2019-06-26 09:33:20 +1200

    FIX: guid validation correct characters range (a-f instead a-z) (see https://github.com/silverstripe/silverstripe-saml/commit/0e28922f7f2f71a0e37ea7128661d6d18fa2cced)

--- code/model/LDAPUtil.php
+++ code/model/LDAPUtil.php
@@ -14,7 +14,7 @@ class LDAPUtil
      */
     public static function validGuid($guid)
     {
-        if (preg_match('/^[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}?$/', $guid)) {
+        if (preg_match('/^[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}?$/', $guid)) {
             return true;
         }
         return false;
